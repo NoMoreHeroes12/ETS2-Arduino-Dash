6fa9fc5297774b7e3af3cc16f1698a385af41680
Ryan Glennon <ryan.glennon.1990@gmail.com>
2026-03-20 12:33:59 +0000

    changes to how telemetry data is stored and draw functions

--- main/main.ino
+++ main/main.ino
@@ -4,7 +4,7 @@
 #include <Adafruit_ILI9341.h>
 #include <Adafruit_TSC2007.h>
 
-/--------  DISPLAY  --------/
+//--------  DISPLAY  --------/
 
 #define TFT_CS 10
 #define TFT_DC 9
@@ -12,25 +12,30 @@
 Adafruit_ILI9341 tft = Adafruit_ILI9341(TFT_CS, TFT_DC);
 Adafruit_TSC2007 ts;
 
-/-------- TOUCH --------/
-/ Sets touch parameters taken from examples TFT examples
+//-------- TOUCH --------/
+// Sets touch parameters taken from examples TFT examples
 #define TS_MINX 150
 #define TS_MINY 130
 #define TS_MAXX 3800
 #define TS_MAXY 4000
 #define TS_MIN_PRESSURE 100
 
-/-------- SCREEN --------/
-/ Just defines number screen currently screen is just a initial screen not really used
-/ TODO
+//-------- SCREEN --------/
+// Just defines number screen currently screen is just a initial screen not really used
+// TODO
 
-#define MIN_SCREEN 0
-#define MAX_SCREEN 3
+#define SCREEN_MAIN   0 // Some fix here
+#define SCREEN_FUEL   1
+#define SCREEN_DAMAGE 2
+#define SCREEN_JOB    3
 
-uint8_t currentScreen = 0;
+#define MIN_SCREEN    0
+#define MAX_SCREEN    3
 
-/-------- SERIAL PARSER --------/
-/ Consider changes in scope? - should this be global variable...
+uint8_t currentScreen = MIN_SCREEN;
+
+//-------- SERIAL PARSER --------/
+// Consider changes in scope? - should this be global variable...
 
 #define KEY_MAX 20
 #define VALUE_MAX 12
@@ -50,48 +55,47 @@ enum ParseState
 
 ParseState parserState = WAIT_START;
 
-/-------- DATA --------/
-/ struct for data
+//-------- DATA --------/
+// struct for data
 
-struct Telemetry
-{
-  char fuel[8] = "000";
-  char max_fuel[8] = "000";
-  char fuel_percent[8] = "000";
-  char fuel_range[8] = "000";
-
-  char dam_cabin[8] = "000";
-  char dam_chassis[8] = "000";
-  char dam_engine[8] = "000";
-  char dam_trans[8] = "000";
-  char dam_wheels[8] = "000";
-
-  char job_dest[8] = "000";
-  char job_dist[8] = "000";
-  char job_inc[8] = "000";
-  char job_time[8] = "000";
-  
+struct Field {
+  const char* key;
+  char value[9];
+  char lastValue[9];
+  const char* label;
+  uint8_t screen;
 };
 
-Telemetry data;
+Field fields[] = {
+  {"fuel", "000", "", "Fuel: ", SCREEN_FUEL},
+  {"max_fuel", "000", "", "Max: ", SCREEN_FUEL},
+  {"fuel_percent", "000", "", "%: ", SCREEN_FUEL},
+  {"fuel_range", "000", "", "Range: ", SCREEN_FUEL},
+  {"dam_cabin", "000", "", "Cabin: ", SCREEN_DAMAGE},
+  {"dam_chassis", "000", "", "Chassis: ", SCREEN_DAMAGE},
+  {"dam_engine", "000", "", "Engine: ", SCREEN_DAMAGE},
+  {"dam_trans", "000", "", "Trans: ", SCREEN_DAMAGE},
+  {"dam_wheels", "000", "", "Wheel: ", SCREEN_DAMAGE},
+  {"job_dest", "000", "", "Dest: ", SCREEN_JOB},
+  {"job_dist", "000", "", "Dist: ", SCREEN_JOB},
+  {"job_inc", "000", "", "Inc: ", SCREEN_JOB},
+  {"job_time", "000", "", "Time: ", SCREEN_JOB}
+};
 
-/-------- LAST DRAWN -------/
-/ Used to for comparison to only udpate screen on changes
+const uint8_t FIELD_COUNT = sizeof(fields) / sizeof(Field);
 
-Telemetry lastDrawn;
 
-/-------- SAFE COPY --------/
-/ Just ensure NULL terminator
+//-------- SAFE COPY --------/
+// Just ensure NULL terminator
 void safeCopy(char *dest, const char *src, size_t size)
 {
   size_t i = 0;
 
   strncpy(dest, src, size - 1);
-  dest[size - 1] = '\0';
-  
+  dest[size - 1] = '\0';  
 }
 
-/-------- SCREEN CHANGE --------/
+//-------- SCREEN CHANGE --------/
 
 void changeScreen(int dir)
 {
@@ -103,58 +107,32 @@ void changeScreen(int dir)
   currentScreen = next;
 
   tft.fillScreen(ILI9341_BLACK);
-  memset(&lastDrawn, 0, sizeof(lastDrawn)); // force redraw
+  
+  // force redraw
+  for (uint8_t i = 0; i < FIELD_COUNT; i++)
+  {
+    fields[i].lastValue[0] = '\0';
+  }
 }
 
-/-------- SERIAL COMMIT --------/
-/ TODO used if statements for ease of initial set up
-/ to review can we reference directly?
-/ also requires 3 changes to add new value - Telemetry, commit and screen
-
+//-------- SERIAL COMMIT --------/
+// TODO used if statements for ease of initial set up
+// to review can we reference directly?
+// lso requires 3 changes to add new value - Telemetry, commit and screen
 
 void commitValue()
 {
-  if (strcmp(keyBuffer, "fuel") == 0)
-    safeCopy(data.fuel, valueBuffer, sizeof(data.fuel));
-
-  else if (strcmp(keyBuffer, "max_fuel") == 0)
-    safeCopy(data.max_fuel, valueBuffer, sizeof(data.max_fuel));
-
-  else if (strcmp(keyBuffer, "fuel_percent") == 0)
-    safeCopy(data.fuel_percent, valueBuffer, sizeof(data.fuel_percent));
-
-  else if (strcmp(keyBuffer, "fuel_range") == 0)
-    safeCopy(data.fuel_range, valueBuffer, sizeof(data.fuel_range));
-
-  else if (strcmp(keyBuffer, "dam_cabin") == 0)
-    safeCopy(data.dam_cabin, valueBuffer, sizeof(data.dam_cabin));
-
-  else if (strcmp(keyBuffer, "dam_chassis") == 0)
-    safeCopy(data.dam_chassis, valueBuffer, sizeof(data.dam_chassis));
-
-  else if (strcmp(keyBuffer, "dam_engine") == 0)
-    safeCopy(data.dam_engine, valueBuffer, sizeof(data.dam_engine));
-
-  else if (strcmp(keyBuffer, "dam_trans") == 0)
-    safeCopy(data.dam_trans, valueBuffer, sizeof(data.dam_trans));
-
-  else if (strcmp(keyBuffer, "dam_wheels") == 0)
-    safeCopy(data.dam_wheels, valueBuffer, sizeof(data.dam_wheels));
-
-  else if (strcmp(keyBuffer, "job_dest") == 0)
-    safeCopy(data.job_dest, valueBuffer, sizeof(data.job_dest));
-
-  else if (strcmp(keyBuffer, "job_dist") == 0)
-    safeCopy(data.job_dist, valueBuffer, sizeof(data.job_dist));
-
-  else if (strcmp(keyBuffer, "job_inc") == 0)
-    safeCopy(data.job_inc, valueBuffer, sizeof(data.job_inc));
-
-  else if (strcmp(keyBuffer, "job_time") == 0)
-    safeCopy(data.job_time, valueBuffer, sizeof(data.job_time));
+  for (uint8_t i = 0; i < FIELD_COUNT; i++)
+  {
+    if (strcmp(keyBuffer, fields[i].key) == 0)
+    {
+      safeCopy(fields[i].value, valueBuffer, sizeof(fields[i].value));
+      return;
+    }
+  }
 }
 
-/-------- SERIAL PARSER --------/
+//-------- SERIAL PARSER --------/
 
 void readSerial()
 {
@@ -203,8 +181,8 @@ void readSerial()
   }
 }
 
-/-------- TOUCH --------/
-/ handles touch screen for changing screen
+//-------- TOUCH --------/
+// handles touch screen for changing screen
 
 void readTouch()
 {
@@ -217,67 +195,38 @@ void readTouch()
   }
 }
 
-/-------- PARTIAL DRAW HELPERS --------/
+//-------- PARTIAL DRAW HELPERS --------/
 
-void drawField(int x, int y, const char *label, const char *value, char *last)
+void drawField(Field &f, int y)
 {
-  if (strcmp(value, last) == 0) return;
+  if (strcmp(f.value, f.lastValue) == 0) return;
 
-  safeCopy(last, value, 8);
+  safeCopy(f.lastValue, f.value, sizeof(f.lastValue));
 
-  / --- EMPTY SPACE ---/ If value is less characters ensures old character dont remain on screen   
-  char emptySpace[8-strlen(value)];
+  // --- EMPTY SPACE ---/ If value is less characters ensures old character dont remain on screen   
+  char emptySpace[9-strlen(f.value)];
   memset(emptySpace, ' ', sizeof(emptySpace));
   emptySpace[sizeof(emptySpace) - 1] = '\0';
   
-  tft.setCursor(x, y);
+  tft.setCursor(0, y);
   tft.setTextColor(ILI9341_WHITE, ILI9341_BLACK);
   tft.setTextSize(3);
 
-  tft.print(label);
+  tft.print(f.label);
   tft.print("     ");
-  tft.setCursor(x + 150, y);
+  tft.setCursor(150, y);
   //tft.print("        ");
   //tft.setCursor(x + 150, y);
-  tft.print(value);
+  tft.print(f.value);
   tft.print(emptySpace);
 
 }
 
-/-------- SCREENS --------/
-
-void drawFuel()
-{
-  
-  drawField(0, 40, "Fuel:", data.fuel, lastDrawn.fuel);
-  drawField(0, 80, "Max:", data.max_fuel, lastDrawn.max_fuel);
-  drawField(0, 120, "%:", data.fuel_percent, lastDrawn.fuel_percent);
-  drawField(0, 160, "Range:", data.fuel_range, lastDrawn.fuel_range);
-}
-
-void drawDamage()
-{
-  drawField(0, 40, "Cabin:", data.dam_cabin, lastDrawn.dam_cabin);
-  drawField(0, 80, "Chassis:", data.dam_chassis, lastDrawn.dam_chassis);
-  drawField(0, 120, "Engine:", data.dam_engine, lastDrawn.dam_engine);
-  drawField(0, 160, "Trans:", data.dam_trans, lastDrawn.dam_trans);
-  drawField(0, 200, "Wheels:", data.dam_wheels, lastDrawn.dam_wheels);
-}
-
-void drawTrip()
-{
-  drawField(0, 40, "Destin:", data.job_dest, lastDrawn.job_dest);
-  drawField(0, 80, "Dist:", data.job_dist, lastDrawn.job_dist);
-  drawField(0, 120, "Income:", data.job_inc, lastDrawn.job_inc);
-  drawField(0, 160, "Time:", data.job_time, lastDrawn.job_time);
-  
-}
-
-/-------- DRAW --------/
+//-------- DRAW --------/
 
 void drawScreen()
 {
-  if (currentScreen == 0) / In itial screen
+  if (currentScreen == SCREEN_MAIN) // Initial screen
   {
     tft.setCursor(0,0);
     tft.setTextSize(3);
@@ -285,17 +234,22 @@ void drawScreen()
     tft.print("Screen 0");
   }
 
-  if (currentScreen == 1)
-    drawFuel();
+  uint16_t space = 40;
+  uint16_t y = 40;
 
-  if (currentScreen == 2)
-    drawDamage();
+  for (uint8_t i = 0; i < FIELD_COUNT; i++)
 
-    if (currentScreen == 3)
-    drawTrip();
+  {
+    if (fields[i].screen == currentScreen)
+    {
+      drawField(fields[i], y);
+      y += space;
+    }
+  }
+  
 }
 
-/-------- SETUP --------/
+//-------- SETUP --------/
 
 void setup()
 {
@@ -314,7 +268,7 @@ void setup()
   Serial.println("Ready");
 }
 
-/-------- LOOP --------/
+//-------- LOOP --------/
 
 void loop()
 {
